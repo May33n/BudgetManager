5d344e1ebdde02cd0ecf45b8492768e360ddc536
May33n <FlagemyEN@seznam.cz>
2022-05-31 06:56:57 +0200

    .

--- BudgetManager2/Form1.cs
+++ BudgetManager2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,46 +14,84 @@ namespace BudgetManager2
     public partial class Form1 : Form
     {
         private readonly BudgetManagerEntities budgetManagerEntities;
-
+        
         public Form1()
         {
             InitializeComponent();
             budgetManagerEntities = new BudgetManagerEntities();
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
         }
+        [DllImport("user32.DLL",EntryPoint = "ReleaseCapture" )]
+        public extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        public extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
 
         private void OpenNewForm(Form newForm, object btnNew)
         {
             newForm.BringToFront();
-            newForm.Parent = Parent;  //??
-            newForm.Show();
+            newForm.ShowDialog();
         }
 
         private void btnNew_Click(object sender, EventArgs e)
         {
-            OpenNewForm(new FormNew(), sender);
+            OpenNewForm(new FormNew(this), sender);
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            //get id of selected row
-            var id = (int)dgvBudgetEntries.SelectedRows[0].Cells["id"].Value;
+            try
+            {
+                //get id of selected row
+                var id = (int)dgvBudgetEntries.SelectedRows[0].Cells["id"].Value;
 
-            //query database for record
-            var entry = budgetManagerEntities.BudgetManagers.FirstOrDefault(q => q.id == id);
+                //query database for record
+                var entry = budgetManagerEntities.BudgetManagers.FirstOrDefault(q => q.id == id);
 
-            //launch FormNew window with data
-            OpenNewForm(new FormNew(entry), sender);
+                //launch FormNew window with data
+                OpenNewForm(new FormNew(entry, this), sender);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please select row");
+            }
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            var id = (int)dgvBudgetEntries.SelectedRows[0].Cells["id"].Value;
-            var entry = budgetManagerEntities.BudgetManagers.FirstOrDefault(q => q.id == id);
+            try
+            {
+                var id = (int)dgvBudgetEntries.SelectedRows[0].Cells["id"].Value;
+                var entry = budgetManagerEntities.BudgetManagers.FirstOrDefault(q => q.id == id);
 
-            //delete entry
-            budgetManagerEntities.BudgetManagers.Remove(entry);
-            budgetManagerEntities.SaveChanges();
-            dgvBudgetEntries.Refresh();
+                //delete entry
+                budgetManagerEntities.BudgetManagers.Remove(entry);
+                budgetManagerEntities.SaveChanges();
+                PopulateGrid();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please select row");
+            }
+        }
+        public void PopulateGrid()
+        {
+            var entries = budgetManagerEntities.BudgetManagers
+                .Select(q => new
+                {
+                    NAME = q.Name,
+                    AMOUNT = q.Amount,
+                    NOTES = q.Notes,
+                    DATE = q.Date,
+                    Id = q.id
+
+                })
+                .ToList();
+
+            dgvBudgetEntries.DataSource = entries;
+            dgvBudgetEntries.Columns[4].Visible = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -65,12 +104,39 @@ namespace BudgetManager2
                    NOTES = q.Notes,
                    DATE = q.Date,
                    Id = q.id
-
                })
                .ToList();
 
             dgvBudgetEntries.DataSource = entries;
             dgvBudgetEntries.Columns[4].Visible = false;
+        }
+
+        private void pnlTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMaximize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
--- BudgetManager2/FormNew.cs
+++ BudgetManager2/FormNew.cs
@@ -14,25 +14,32 @@ namespace BudgetManager2
     {
         private readonly BudgetManagerEntities budgetManagerEntities;
         private bool isEditMode;
+        private Form1 _form1;
 
-        public FormNew()
+        public FormNew(Form1 form1 = null)
         {
             InitializeComponent();
+            _form1 = form1;
             budgetManagerEntities = new BudgetManagerEntities();
             lblTitle.Text = "Add new entry";
             isEditMode = false;
+            this.Text = string.Empty;
+            this.ControlBox = false;
         }
 
-        public FormNew(BudgetManager entryToEdit)
+        public FormNew(BudgetManager entryToEdit, Form1 form1 = null)
         {
             InitializeComponent();
+            _form1 = form1;
             budgetManagerEntities = new BudgetManagerEntities();
             lblTitle.Text = "Edit entry";
             PopulateFields(entryToEdit);
             isEditMode = true;
+            this.Text = string.Empty;
+            this.ControlBox = false;
         }
 
-        private void PopulateFields(BudgetManager entryToEdit)
+        public void PopulateFields(BudgetManager entryToEdit)
         {
             lblId.Text = entryToEdit.id.ToString();
             txtBoxName.Text = entryToEdit.Name;
@@ -43,42 +50,65 @@ namespace BudgetManager2
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            if (isEditMode)
+            try
             {
-                var id = int.Parse(lblId.Text);
-                var entryToEdit = budgetManagerEntities.BudgetManagers.FirstOrDefault(q => q.id == id);
-                entryToEdit.Name = txtBoxName.Text;
-                entryToEdit.Amount = int.Parse(txtBoxAmount.Text);
-                entryToEdit.Notes = txtBoxNotes.Text;
-                entryToEdit.Date = dt.Value;
+                bool isValid = true;
+
+                if (string.IsNullOrWhiteSpace(txtBoxAmount.Text))
+                {
+                    isValid = false;
+                    MessageBox.Show("Please enter Amount");
+                }
+
+                if (isValid)
+                {
+                    if (isEditMode)
+                    {
+                        var id = int.Parse(lblId.Text);
+                        var entryToEdit = budgetManagerEntities.BudgetManagers.FirstOrDefault(q => q.id == id);
+                        entryToEdit.Name = txtBoxName.Text;
+                        entryToEdit.Amount = int.Parse(txtBoxAmount.Text);
+                        entryToEdit.Notes = txtBoxNotes.Text;
+                        entryToEdit.Date = dt.Value;
+                    }
+                    else
+                    {
+                        var budgetManager = new BudgetManager();
+
+                        string name = txtBoxName.Text;
+                        string notes = txtBoxNotes.Text;
+                        var date = dt.Value;
+                        int amount = int.Parse(txtBoxAmount.Text);
+
+                        budgetManager.Name = name;
+                        budgetManager.Notes = notes;
+                        budgetManager.Date = date;
+                        budgetManager.Amount = amount;
+
+                        budgetManagerEntities.BudgetManagers.Add(budgetManager);
+                    }
+                }
                 budgetManagerEntities.SaveChanges();
+                _form1.PopulateGrid();
+
+                MessageBox.Show("OK");
+                Close();
             }
-            else
+            catch (Exception ex)
             {
-                var budgetManager = new BudgetManager();
-
-                string name = txtBoxName.Text;
-                string notes = txtBoxNotes.Text;
-                var date = dt.Value;
-                int amount = int.Parse(txtBoxAmount.Text);
-
-                budgetManager.Name = name;
-                budgetManager.Notes = notes;
-                budgetManager.Date = date;
-                budgetManager.Amount = amount;
-
-                budgetManagerEntities.BudgetManagers.Add(budgetManager);
-                budgetManagerEntities.SaveChanges();
+                MessageBox.Show($"chyba { ex.Message}");
             }
-            
-            budgetManagerEntities.SaveChanges();
-
-            MessageBox.Show("OK");
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void pnlTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form1.ReleaseCapture();
+            Form1.SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
